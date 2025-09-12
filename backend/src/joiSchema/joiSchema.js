b4740c113fa06cd4813c7c5e3be786ffabc5f846
omkoli77 <koliom19@gmail.com>
2025-09-12 17:08:57 +0530

    add joi schema

---
+++ backend/src/joiSchema/joiSchema.js
@@ -0,0 +1,8 @@
+import joi from 'joi';
+
+export const userSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().required(),
+    password: joi.string().required(),
+    location: joi.string().required(),
+});
