1d2d0aa1694e422130d52ee41766387add9c0dd6
Ammisetty Chamundeswari <131233976+Ammisettychamu33@users.noreply.github.com>
2025-10-25 21:23:54 +0530

    Add files via upload

---
+++ frontend/utils/api.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:3000/api/v1';
+
+const api = axios.create({
+    baseURL: API_BASE_URL,
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('accessToken');
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            localStorage.clear();
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default api;
